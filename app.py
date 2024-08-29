f4b538c7a06f10c4bf3cc17a9dc77a29b80df2df
Haris Sohail <harissohail.cs@gmail.com>
2024-08-29 15:16:51 +0500

    Add tests, change mod and divide functionality to handle division by zero

--- app.py
+++ app.py
@@ -9,7 +9,13 @@ def multiply(x, y):
     return x * y
 
 def divide(x, y):
+    if y == 0:
+        return "Undefined"
+    
     return x / y
 
 def mod(x, y):
+    if(y == 0):
+        return "Undefined"
+    
     return x % y
